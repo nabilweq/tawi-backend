612fb1d70e86d19769716bd3648b42da70d18ecc
nabilweq <nabeeltkanr@gmail.com>
2022-03-04 08:18:17 +0530

    Cors error second

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ connectDB();
 // Init Middleware
 app.use(express.json());
 
+// To remove CROS (cross-resource-origin-platform) problem
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // to allow all client we use *
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS,GET,POST,PUT,PATCH,DELETE"
+    ); //these are the allowed methods
+    res.setHeader("Access-Control-Allow-Headers", "*"); // allowed headers (Auth for extra data related to authoriaztion)
+    next();
+});
+
 app.get('/', (req, res, ) => {
     res.send('API Running, Please use /api/users');
 });
